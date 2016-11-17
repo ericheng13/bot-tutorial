a74b9f5d0294932bd9796e877fde6ff4e9d22bca
ericheng13 <ericheng13@gmail.com>
2016-11-17 16:19:12 -0500

    concat nuke response

--- bot.js
+++ bot.js
@@ -12,8 +12,7 @@ function respond() {
                       + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
                       + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
                       + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-      nukeResponse = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-                      + "- -..-^~~~^-..- -\n-.~ - - - - - ~.-\n(;: - - - - - :;)\n-(: - - - - - :)-\n-- ':.- - -.:' --\n-- - - | | - - --\n-- - (=====) - --\n-- - - | | - - --\n-- - - | | - - --\n-- - - | | - - --\n-- -((/ - \\))- --";
+      nukeResponse = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n- -..-^~~~^-..- -\n-.~ - - - - - ~.-\n(;: - - - - - :;)\n-(: - - - - - :)-\n-- ':.- - -.:' --\n-- - - | | - - --\n-- - (=====) - --\n-- - - | | - - --\n-- - - | | - - --\n-- - - | | - - --\n-- -((/ - \\))- --";
                    //"   ..-^~~~^-..   \n .~           ~. \n(;:           :;)\n (:           :) \n   ':.-   -.:'   \n       | |       \n     (=====)     \n       | |\n       | |-------\n       | |-------\n    ((/   \))----";
 
   if(request.text && botRegex.test(request.text)) {
